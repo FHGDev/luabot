ba4e46c0733b78d5b3de261e59a9c907f7439009
FHGDev <gideon.lingle14@gmail.com>
2018-05-21 10:04:51 -0500

    Create package.lua

---
+++ package.lua
@@ -0,0 +1,9 @@
+return {
+  name = "FHGDev/TestBot",
+  version = "0.0.1",
+  private = true, -- This prevents us from accidentally publishing this package.
+  dependencies = {
+    "luvit/require",
+    "luvit/discordia"
+  }
+}
